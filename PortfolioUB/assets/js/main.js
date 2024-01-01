dc7c15c4b052cb43b34f4e7bed3db598321aa256
Utkarsh <utkarshbhujbal02@gmail.com>
2024-01-01 18:57:11 +0530

    Initial Commit

---
+++ PortfolioUB/assets/js/main.js
@@ -0,0 +1,12 @@
+AOS.init();
+
+AOS.init({
+  offset: 120, // original trigger point
+  delay: 0, // with step 50ms
+  duration: 700, // step 50ms
+  easing: "ease",
+  once: false, 
+  mirror: false, 
+  anchorPlacement: "top-bottom", 
+});
+
